3a0cff550fb0f84eb1f5ac0be089822f950bde85
emile kamana <emileizere@gmail.com>
2020-12-12 23:23:50 +0200

    create the departmentdao interface for the functions to be implemented

---
+++ src/main/java/dao/DepartmentDao.java
@@ -0,0 +1,24 @@
+package dao;
+
+import models.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    //create
+    void add(Department department);
+    //void addUserToDepartment(User user, Department department);
+
+    //read
+    List<Department> getAll();
+
+    // find by ID
+    Department findById(int id);
+
+    //update
+    //void update(int id, String name, String bio);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+}
