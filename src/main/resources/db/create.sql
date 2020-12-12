491eaee9f1258410d5dd5e4d28302c4fafc78cd7
emile kamana <emileizere@gmail.com>
2020-12-12 12:40:27 +0200

    create the user class

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE organisational;
 \c organisation;
 
-CREATE TABLE users(id serial PRIMARY KEY, name varchar, position varchar, role varchar, departmentid int);
+CREATE TABLE users(id serial PRIMARY KEY, name varchar, bio varchar, position varchar, role varchar, departmentid int);
 
 CREATE TABLE news(id serial PRIMARY KEY, title varchar, content varchar, departmentid int);
 
-CREATE TABLE departments(id serial PRIMARY KEY, name varchar, description varchar);
+CREATE TABLE departments(id serial PRIMARY KEY, name varchar, bio varchar);
 
 CREATE DATABASE organisational_test WITH TEMPLATE organisational;
