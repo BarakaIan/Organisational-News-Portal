ef6bd1b0485374955846a73de2f753b392097f72
emile kamana <emileizere@gmail.com>
2020-12-13 10:05:51 +0200

    create the newsdao interface for the functions to be implemented

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,5 +1,24 @@
 package dao;
 
-public interface NewsDao {
+import models.News;
 
+import java.util.List;
+
+public interface NewsDao {
+    //create
+    void add(News news);
+    //void addNewsToDepartment(News news, Department department);
+
+    //read
+    List<News> getAll();
+
+    // find by ID
+    News findById(int id);
+
+    //update
+    //void update(int id, String name, String bio);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
