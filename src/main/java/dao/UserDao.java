1d2754627d291f14067cd844c7e901a0fe149a69
emile kamana <emileizere@gmail.com>
2020-12-13 10:04:21 +0200

    create the userdao interface for the functions to be implemented

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,24 @@
 package dao;
 
+import models.User;
+
+import java.util.List;
+
 public interface UserDao {
+    //create
+    void add(User user);
+    //void addUserToDepartment(User user, Department department);
+
+    //read
+    List<User> getAll();
+
+    // find by ID
+    User findById(int id);
+
+    //update
+    //void update(int id, String name, String bio);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
